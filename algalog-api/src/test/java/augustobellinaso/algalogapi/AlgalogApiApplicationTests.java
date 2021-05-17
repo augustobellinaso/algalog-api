f8c86e8133eff4a4e8e983798c205a2fc3ca1339
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-05-17 10:38:08 -0300

    Criando o projeto

---
+++ algalog-api/src/test/java/augustobellinaso/algalogapi/AlgalogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package augustobellinaso.algalogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgalogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
