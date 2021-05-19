71dd6a6adc08d44fea76d0f25c5613be98d48450
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-05-19 13:55:38 -0300

    Usando JPA

--- algalog-api/src/main/java/augustobellinaso/algalogapi/controller/ClienteController.java
+++ algalog-api/src/main/java/augustobellinaso/algalogapi/controller/ClienteController.java
@@ -4,27 +4,19 @@ import augustobellinaso.algalogapi.domain.model.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @RestController
 public class ClienteController {
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @GetMapping("/clientes")
     public List<Cliente> listar() {
-        Cliente c1 = new Cliente();
-        c1.setId(1L);
-        c1.setNome("João");
-        c1.setEmail("joao@joao.com");
-        c1.setTelefone("5151515");
-
-        Cliente c2 = new Cliente();
-        c2.setId(2L);
-        c2.setNome("Maria");
-        c2.setEmail("Maria@joao.com");
-        c2.setTelefone("51515151241515");
-
-
-        return Arrays.asList(c1, c2);
+        return entityManager.createQuery("from Cliente", Cliente.class)
+                .getResultList();
     }
 }
--- algalog-api/src/main/java/augustobellinaso/algalogapi/domain/model/Cliente.java
+++ algalog-api/src/main/java/augustobellinaso/algalogapi/domain/model/Cliente.java
@@ -1,15 +1,27 @@
 package augustobellinaso.algalogapi.domain.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
+
     private String nome;
+
     private String email;
+
+    @Column(name = "fone")
     private String telefone;
 
 
