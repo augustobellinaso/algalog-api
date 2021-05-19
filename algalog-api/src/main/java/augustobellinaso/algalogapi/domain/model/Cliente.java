7a6a59e7d7e4c2985fe489ef70f6e5556a1d9f55
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2021-05-19 14:47:47 -0300

    Validando dados com Bean Validation

--- algalog-api/src/main/java/augustobellinaso/algalogapi/domain/model/Cliente.java
+++ algalog-api/src/main/java/augustobellinaso/algalogapi/domain/model/Cliente.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -17,13 +20,18 @@ public class Cliente {
     @EqualsAndHashCode.Include
     private Long id;
 
+    @NotBlank
+    @Size(max = 60)
     private String nome;
 
+    @NotBlank
+    @Email
+    @Size(max = 255)
     private String email;
 
     @Column(name = "fone")
+    @NotBlank
+    @Size(max = 20)
     private String telefone;
-
-
 
 }
